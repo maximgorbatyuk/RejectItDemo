57cc6f81b0e0f4be32af462d6ea86d7bfd717d81
Maxim <maxim.gorbatyuk@outlook.com>
2020-02-16 15:26:24 +0600

    Have added karma and prettier

--- src/RejectItDemo/Controllers/WeatherForecastController.cs
+++ src/RejectItDemo/Controllers/WeatherForecastController.cs
@@ -32,7 +32,6 @@ namespace RejectItDemo.Controllers
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
-
             })
             .ToArray();
         }
